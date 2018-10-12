e8eb85b4ffd87c0fa1d79f9f2e3b54a658472c45
何佳农 <jianonghe@gmail.com>
2018-10-12 08:24:00 +0000

    为兼容线上环境做出优化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,8 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+
         //注册模型监视器
         \App\Models\User::observe(\App\Observers\UserObserver::class);
+
+        //由于Laravel 默认使用 utf8mb4 字符，如果线上服务器版本低于5.7.7，则需要设置默认字符串长度
+        Schema::defaultStringLength(191);
     }
 
     /**
