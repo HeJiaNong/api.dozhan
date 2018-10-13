5bd3eb19492d0f11dbb89c107975732b09c72ad4
何佳农 <jianonghe@gmail.com>
2018-10-13 11:08:22 +0000

    关闭api访问限制

--- routes/api.php
+++ routes/api.php
@@ -33,9 +33,9 @@ $api = app(\Dingo\Api\Routing\Router::class);
 
 $api->version('v1',['namespace' => 'App\Http\Controllers\Api'],function ($api){
     $api->group([
-        'middleware' => 'api.throttle', //DingoApi 已经为我们提供了调用频率限制的中间件 api.throttle
-        'limit' => config('api.rate_limits.sign.limit'),    //接口频率限制
-        'expires' => config('api.rate_limits.sign.expires'),
+//        'middleware' => 'api.throttle', //DingoApi 已经为我们提供了调用频率限制的中间件 api.throttle
+//        'limit' => config('api.rate_limits.sign.limit'),    //接口频率限制
+//        'expires' => config('api.rate_limits.sign.expires'),
     ], function ($api) {
         //------------------游客可以访问的接口-----------------------
         $api->get('test','TestController@store')->name('api.test.store');
@@ -46,7 +46,7 @@ $api->version('v1',['namespace' => 'App\Http\Controllers\Api'],function ($api){
         //注册用户
         $api->get('user','UserController@store')->name('api.user.store');
 
-        //------------------需要 token 验证的接口-----------------------
+        //------------------需要 token 验证的接口--------  ---------------
 //        $api->group(['middleware' => 'api.auth'], function($api) {
 //            // 当前登录用户信息
 //            $api->get('user', 'UserController@me')
