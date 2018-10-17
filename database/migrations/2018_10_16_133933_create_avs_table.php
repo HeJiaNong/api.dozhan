9d6531fb46b201469e2985da4ee75b45f104bfaf
何佳农 <jianonghe@gmail.com>
2018-10-17 14:16:07 +0000

    修改avs表的album_id为可为空

--- database/migrations/2018_10_16_133933_create_avs_table.php
+++ database/migrations/2018_10_16_133933_create_avs_table.php
@@ -18,7 +18,7 @@ class CreateAvsTable extends Migration
             $table->string('name')->unique()->index()->comment('视频名称');
             $table->string('description')->comment('描述');
             $table->integer('user_id')->unsigned()->comment('用户ID');
-            $table->integer('album_id')->unsigned()->comment('专辑ID');
+            $table->integer('album_id')->unsigned()->nullable()->comment('专辑ID');
             $table->integer('url_id')->unsigned()->comment('视频地址ID');
             $table->integer('cover_id')->unsigned()->comment('封面地址ID');
             $table->integer('category_id')->unsigned()->comment('分类ID');
