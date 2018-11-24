233c7a4bd52d238ae9247daaa781430d0f9e230c
HeJiaNong <jianonghe@gmail.com>
2018-11-24 08:13:16 +0000

    为banner添加链接地址

--- config/site.php
+++ config/site.php
@@ -2,10 +2,23 @@
 
 return [
     'banners' => [
-        'http://phcczptg4.bkt.clouddn.com/seeder/banner/1.jpg' => 'welcome',
-        'http://phcczptg4.bkt.clouddn.com/seeder/banner/2.jpg' => 'dozhan',
-        'http://phcczptg4.bkt.clouddn.com/seeder/banner/3.jpg' => '666',
+        [
+            'img' => 'http://phcczptg4.bkt.clouddn.com/seeder/banner/1.jpg',
+            'link' => '#',
+            'desc' => 'welcome',
+        ],
+        [
+            'img' => 'http://phcczptg4.bkt.clouddn.com/seeder/banner/2.jpg',
+            'link' => '#',
+            'desc' => 'dozhan',
+        ],
+        [
+            'img' => 'http://phcczptg4.bkt.clouddn.com/seeder/banner/3.jpg',
+            'link' => '#',
+            'desc' => '嘿嘿',
+        ],
     ],
+
     'links' => [
         '百度' => 'http://www.baidu.com',
         '新浪' => 'http://www.sina.com',
