54e2515a470e6959cc896b312dd5f32f142f0f0f
HeJiaNong <jianonghe@gmail.com>
2018-12-04 07:11:46 +0000

    删除前端资源

--- resources/views/common/error.blade.php
+++
@@ -1,5 +0,0 @@
-@if (count($errors))
-        @foreach($errors->all() as $error)
-            <h1>{{ $error }}</h1>
-        @endforeach
-@endif
--- resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-
-@extends('layouts.app')
-
-@section('content')
-    首页
-@endsection
